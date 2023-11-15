c565b69a9d5a3aa780deeb2a45230e1d92eff337
Julien Ramboz <ramboz@adobe.com>
2023-11-15 08:59:19 +0100

    chore: various refactoring

--- aep-sync/lib/index.js
+++ aep-sync/lib/index.js
@@ -1,21 +1,27 @@
 const { mkdir, writeFile } = require('fs').promises;
+const { Base64 } = require('js-base64');
 const { JSDOM } = require('jsdom');
 const { dirname } = require('path');
-const { Base64 } = require('js-base64');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const http = require('@actions/http-client');
-const { context: imsContext, getToken, getTokenData } = require('@adobe/aio-lib-ims');
+const { context: imsContext, getToken } = require('@adobe/aio-lib-ims');
 
 const BOT_NAME = 'aem-experimentation-aep-sync';
+
+const BOT_USER = {
+  name: BOT_NAME,
+  email: 'ramboz@adobe.com',
+};
+
 const client = new http.HttpClient(BOT_NAME);
 
-function getManifestPath(pagePath) {
-  return `experiments${`${pagePath.replace(/\.md$/, '')}`}.manifest.json`;
+function getManifestPath(root, pagePath) {
+  return `${root.replace(/^\//, '')}${`${pagePath.replace(/\.md$/, '')}`}.manifest.json`;
 }
 
-async function getExperimentIdFromDocument(domain, path) {
-  const url = `${domain}${path.replace(/index.md$/, '').replace(/\.md$/, '')}`;
+async function getExperimentIdFromDocument(host, path) {
+  const url = `${host}${path.replace(/index.md$/, '').replace(/\.md$/, '')}`;
   const response = await client.get(url);
   const htmlString = await response.readBody();
   const dom = new JSDOM(htmlString);
@@ -32,19 +38,13 @@ async function getExperimentIdFromDocument(domain, path) {
 async function getShaForExistingManifest(octokit, owner, repo, ref, path) {
   try {
     const result = await octokit.rest.repos.getContent({ owner, repo, ref, path });
-    console.log(111, result);
     return result.data.sha;
   } catch (err) {
-    console.log(222, err);
     return undefined;
   }
 }
 
 async function deleteManifestFromRepo(octokit, owner, repo, ref, path, sha, experimentId) {
-  const user = {
-    name: BOT_NAME,
-    email: 'ramboz@adobe.com',
-  };
 
   return octokit.rest.repos.deleteFile({
     owner,
@@ -53,8 +53,8 @@ async function deleteManifestFromRepo(octokit, owner, repo, ref, path, sha, expe
     path,
     sha,
     message: `chore: delete manifest for removed AEP experiment ${experimentId}`,
-    committer: user,
-    author: user,
+    committer: BOT_USER,
+    author: BOT_USER,
   });
 }
 
@@ -88,11 +88,6 @@ async function addManifestToRepo(octokit, owner, repo, ref, path, manifest, sha)
   await mkdir(dirname(path), { recursive: true });
   await writeFile(path, JSON.stringify(manifest));
 
-  const user = {
-    name: BOT_NAME,
-    email: 'ramboz@adobe.com',
-  };
-
   const res = await octokit.rest.repos.createOrUpdateFileContents({
     owner,
     repo,
@@ -100,8 +95,8 @@ async function addManifestToRepo(octokit, owner, repo, ref, path, manifest, sha)
     path,
     message: `chore: update AEP experiment manifest cache for ${manifest.id}`,
     content: Base64.encode(JSON.stringify(manifest)),
-    committer: user,
-    author: user,
+    committer: BOT_USER,
+    author: BOT_USER,
     sha,
   });
   return res.data.content.path;
@@ -109,38 +104,32 @@ async function addManifestToRepo(octokit, owner, repo, ref, path, manifest, sha)
 
 async function run() {
   try {
-    console.log('Start…');
+    // Secrets
     const client_id = core.getInput('aio_console_client_id', { required: true });
     const client_secret = core.getInput('aio_console_client_secret', { required: true });
     const ims_org_id = core.getInput('aio_console_ims_org_id', { required: true });
     const technical_account_email = core.getInput('aio_console_technical_account_email', { required: true });
     const technical_account_id = core.getInput('aio_console_technical_account_id', { required: true });
+    const patToken = core.getInput('git_pat_token', { required: true });
     
+    // Variables
     const git_repo = core.getInput('git_repo', { required: true });
     const [owner, repo] = git_repo.split('/');
-    const ref = core.getInput('git_ref', { required: true });
-
-    console.log('Params…');
-    const prodHost = core.getInput('prod_host', { required: false })
+    const ref = core.getInput('git_ref');
+    const pagePath = core.getInput('resource_path', { required: true });
+    const prodHost = core.getInput('prod_host')
       || `https://${ref}--${repo}--${owner}.hlx.live`;
 
-      console.log('Prod Host', prodHost);
-
-    const pagePath = core.getInput('resource_path', { required: true });
-
-    const patToken = core.getInput('git_pat_token', { required: true });
-
-
-    const manifestPath = getManifestPath(pagePath);
-    console.log('Manifest Path', manifestPath);
+    const manifestPath = getManifestPath('experiments', pagePath);
+    console.log('Manifest path:', manifestPath);
 
     const octokit = github.getOctokit(patToken);
 
     const experimentId = await getExperimentIdFromDocument(prodHost, pagePath);
-    console.log('Experiment Id', experimentId);
+    console.log('Experiment id:', experimentId);
     const manifestSha = await getShaForExistingManifest(octokit, owner, repo, ref, manifestPath)
-    console.log('Manifest Sha', manifestSha);
     if (!experimentId && manifestSha) {
+      console.log('Deleting stray manifest for experiment:', experimentId);
       await deleteManifestFromRepo(octokit, owner, repo, ref, manifestPath, manifestSha, experimentId);
       return;
     }
@@ -153,9 +142,7 @@ async function run() {
       ims_org_id,
     });
     const config = await getExperimentConfigFromAep(experimentId, ims_org_id, accessToken);
-    console.log('Experiment Config', config);
     const manifest = await convertExperimentConfigToManifest(config);
-    console.log('Experiment Manifest', manifest);
     await addManifestToRepo(octokit, owner, repo, ref, manifestPath, manifest, manifestSha, patToken);
   } catch (err) {
     core.setFailed(err.message);
