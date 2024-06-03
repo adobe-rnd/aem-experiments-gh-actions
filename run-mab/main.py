fc718f4c459332da8b826422651cade3ac0bbe46
Julien Ramboz <ramboz@adobe.com>
2024-06-03 12:00:15 -0700

    feat: add run mab github action

--- run-mab/main.py
+++ run-mab/main.py
@@ -2,8 +2,12 @@
 
 # coding: utf-8
 
+import os
 import sys
 
-print(sys.argv[1:]);
+print(sys.argv[1:])
+
+for key, value in os.environ.items():
+    print('{}: {}'.format(key, value))
 
 # TODO: add logic here
