7936d11f9730ce8ea319caf25a29e22a1800bfeb
Julien Ramboz <ramboz@adobe.com>
2024-06-03 14:09:48 -0700

    feat: add run mab github action

--- run-mab/main.py
+++ run-mab/main.py
@@ -11,3 +11,7 @@ for key, value in os.environ.items():
     print('{}: {}'.format(key, value))
 
 # TODO: add logic here
+
+# import json
+# data = json.loads('{"one" : "1", "two" : "2", "three" : "3"}')
+# print(data['two'])  # or `print data['two']` in Python 2
