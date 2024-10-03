8c5a71d29f83df9659522c4fddc86f017c3baab8
Dereje Dilnesaw <dilnesaw@adobe.com>
2024-10-02 17:38:48 -0700

    read json file

--- fetch-rum-insights/lib/index.js
+++ fetch-rum-insights/lib/index.js
@@ -66,7 +66,7 @@ async function run() {
     const context = getActionContext();
     const experimentInsights = {};
     // const allChunks = await fetchBundles(context.domain, context.days, context.domainKey);
-    const fs = require('fs');
+    // const fs = require('fs');
     const fileContent = fs.readFileSync('fetch-rum-insights/lib/xwalk-test.json', 'utf8');
     const allChunks = JSON.parse(fileContent);
     const eventFilters = {
